1ceccfda2dc0c8533e32bf3323d012039a5fe288
alpha-bird <alpha.birdprime@gmail.com>
2021-12-14 13:34:16 -0500

    cors issue

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
+server.use(cors());
 server.use(middlewares);
 console.log(router);
 server.use(router);
